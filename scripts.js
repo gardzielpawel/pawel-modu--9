c8beeea2789b70685f794056709fbf64fe1d74be
Paweł Gardziel <gardzielpawel@gmail.com>
2018-01-19 10:30:44 +0100

    moduł 9.2 wer2

--- scripts.js
+++ scripts.js
@@ -1,14 +1,3 @@
-function getTriangleArea(a, h) {
-	if ( a > 0 && h > 0) {
-  		return a*h/2;
-	}else {
-    		return 'Nieprawidlowe_dane';
-  }
-}
-   
-var triangle1Area = getTriangleArea(5, 25);
-var triangle2Area = getTriangleArea(13, 6);
-var triangle3Area = getTriangleArea(-3, 4);
 
 
 var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
