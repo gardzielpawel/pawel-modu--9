ea43d5ff043fe1ffb9cbc0662633b9a1bf11eda4
Paweł Gardziel <gardzielpawel@gmail.com>
2018-01-20 11:32:43 +0100

    moduł 9.2 ver3

--- scripts.js
+++ scripts.js
@@ -1,19 +1,14 @@
 
-
 var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
 var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
 var allNames = femaleNames.concat(maleNames);
 
 console.log(allNames);
 
-var newName = 'Marian'; {
-	if (allNames.indexOf(newName) === -1){
-    		return 'true';
-  	}else {
-    		return 'false';
-  }  
-}
+var newName = 'Marian'; 
 
-var x = test.push('newName');
+if (allNames.indexOf(newName) === -1) {
+  allNames.push(newName);
+} 
 
 console.log(allNames);
