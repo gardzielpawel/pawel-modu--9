4f524e7a065bc56bf04857783cce42d72733734e
Paweł Gardziel <gardzielpawel@gmail.com>
2018-01-20 19:49:24 +0100

    moduł 9.5

--- scripts.js
+++ scripts.js
@@ -1,10 +1,6 @@
-function drawTree(w) {
-	for (var i = 0; i < w; i++) {
-	var star = ' ';
-	
-	for (var j = 0; j <= i; j++) {
-	star += '*';
-	}
-	console.log(star);
-	}
+var elements = document.getElementsByClassName("button");
+for (var k = 0; k < elements.length; k++) {
+	alert(elements[k].innerText);
 }
+
+console.log(elements.length);
