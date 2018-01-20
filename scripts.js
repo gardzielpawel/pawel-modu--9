d4635e0ca034cfbaa59316571f119e711d5f8d2d
Paweł Gardziel <gardzielpawel@gmail.com>
2018-01-20 18:56:33 +0100

    moduł 9.3

--- scripts.js
+++ scripts.js
@@ -1,14 +1,13 @@
+var text = 'Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.' ;
+var dinosaur = 'triceratops' ;
+var dinosaurUpperCase = dinosaur.toUpperCase();
+var dinosaurReplace = text.replace('Velociraptor', dinosaurUpperCase);
+var dinosaurHalf = dinosaurReplace.slice(0, dinosaurReplace.length/2);
 
-var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
-var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
-var allNames = femaleNames.concat(maleNames);
 
-console.log(allNames);
 
-var newName = 'Marian'; 
 
-if (allNames.indexOf(newName) === -1) {
-  allNames.push(newName);
-} 
 
-console.log(allNames);
+console.log(dinosaurUpperCase);
+console.log(dinosaurReplace);
+console.log(dinosaurHalf);
